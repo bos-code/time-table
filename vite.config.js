a122252c11727b41ca29ccbb6c4e0884c25e2daa
Henry chidera <chidera9713@gmail.com>
2026-03-31 23:26:29 +0100

    feat: add server proxy configuration for API requests

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,14 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:8000",
+        changeOrigin: true,
+      },
+    },
+  },
   build: {
     // Fix: Split vendor chunks (e.g., react, firebase, etc.)
     rollupOptions: {
